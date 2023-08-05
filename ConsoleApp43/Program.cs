334abbea52d48285f8876b27a4b54e07744a4d04
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-05 21:18:17 +0330

    Add files via upload

---
+++ ConsoleApp43/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp43
+{
+   public class EmployeeTest
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Employees before instanstiation:{Employee.Count}");
+
+            var e1 = new Employee("Susan", "Baker");
+            var e2 = new Employee("Bob", "Blue");
+
+
+            Console.WriteLine($"\nEmployees after instantiation:{Employee.Count}");
+
+
+            Console.WriteLine($"\nEmployee 1:{e1.FirstName}{e1.LastName}");
+            Console.WriteLine($"Employee 2:{e2.FirstName}{e2.LastName}");
+            Console.ReadLine();
+
+
+            e1 = null;
+            e2 = null;
+        }
+    }
+}
